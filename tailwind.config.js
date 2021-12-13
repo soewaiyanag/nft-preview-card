9829884526dca685b484e4caa8042527225e888d
soewaiyanag <soewaiyanag@gmail.com>
2021-12-13 16:46:02 +0630

    finished the challenge

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        "dark-blue-1": "hsl(217, 54%, 11%)",
+        "dark-blue-2": "hsl(216, 50%, 16%)",
+        "dark-blue-3": "hsl(215, 32%, 27%)",
+        "blue-soft": "hsl(215, 51%, 70%)",
+        cyan: "hsl(178, 100%, 50%)",
+      },
+      fontFamily: {
+        outfit: ["Outfit", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
